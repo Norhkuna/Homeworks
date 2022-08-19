69b274fd19f786d2c0823faf0bdaef83818286e8
Norhkuna <konsta.pylkko@gmail.com>
2022-08-19 10:53:32 +0300

    Initial commit for Homework

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+        // homework 1
+        //Question 1:
+        //      State two(2) fundamental operations and two(2) compound operations
+        //      Fundamental operations = addition and subtraction
+        //      Compound operations = simulate a situation in real life or analyze data
+
+        //Question 2: State three essential features of a programming language
+        //      1. Reserved words always have the same meaning (eg. int always means an integer)
+        //      2. Requirement of syntax while programming doesn't change
+        //         (eg. using a semicolon is always necessary at the end of statements)
+        //      3. The language allows programmers to write their code
+        //         according to structured programming concepts, to avoid creating errors.
+
+        //Question 3: Outline the operation of a compiler
+        //          A compiler translates any given language's source code into machine code
+        //          which can be understood by the computer
+
+        public static void main(String[] args)
+        {
+            System.out.println("hi mom!");
+        }
+    }
+}
